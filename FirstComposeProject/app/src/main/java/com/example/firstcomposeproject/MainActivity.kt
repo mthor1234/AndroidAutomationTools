5ebdbc3aa8d828059a8736444909e29e3473b490
Mitchell Thornton <MThornton@tandemdiabetes.com>
2021-03-25 21:38:35 -0700

    Step 5

--- FirstComposeProject/app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ FirstComposeProject/app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.FontWeight.Companion.Black
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.firstcomposeproject.ui.theme.FirstComposeProjectTheme
@@ -41,24 +46,18 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
 }
 
-@Preview(showBackground = true, name = "Text preview")
 @Composable
-fun DefaultPreview() {
-    MyApp{
-        MyScreenContent()
-    }
-}
-
-@Composable
-fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" }){
     val counterState = remember { mutableStateOf(0) }
 
-    Column {
-        for(name in names){
-            Greeting(name = name)
-            Divider(color = Color.Black)
+    Column(modifier = Modifier.fillMaxHeight()) {
+        Column(modifier = Modifier.weight(1f)) {
+            for (name in names) {
+                Greeting(name = name)
+                Divider(color = Color.Black)
+            }
+            Divider(color = Color.Transparent, thickness = 32.dp)
         }
-        Divider(color = Color.Transparent, thickness = 32.dp)
         Counter(
             count = counterState.value,
             updateCount = { newCount ->
@@ -72,5 +71,23 @@ fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
 fun Counter(count: Int, updateCount: (Int) -> Unit){
     Button(onClick = {updateCount(count+1) }){
         Text("I've been clicked $count times")
+    }
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name ->
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+    }
+}
+
+@Preview(showBackground = true, name = "Text preview")
+@Composable
+fun DefaultPreview() {
+    MyApp{
+        MyScreenContent()
     }
 }
