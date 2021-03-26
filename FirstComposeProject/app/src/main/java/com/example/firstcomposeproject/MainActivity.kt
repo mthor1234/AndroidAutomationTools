380da53f2d4252c008a8164414c256062863347d
Mitchell Thornton <MThornton@tandemdiabetes.com>
2021-03-25 21:08:33 -0700

    Step 3 Completed

--- FirstComposeProject/app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ FirstComposeProject/app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -3,36 +3,58 @@ package com.example.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FirstComposeProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
+fun MyApp(content: @Composable () -> Unit) {
+    FirstComposeProjectTheme {
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
 }
 
 @Preview(showBackground = true, name = "Text preview")
 @Composable
 fun DefaultPreview() {
-    FirstComposeProjectTheme {
-        Greeting("Android")
+    MyApp{
+        MyScreenContent()
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    Column {
+        for(name in names){
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
     }
 }
